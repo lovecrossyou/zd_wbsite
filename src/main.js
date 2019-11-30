f8ba516181fd5384797db4c57a6f1e30c9b8d6a2
sally <sally@sallydeMacBook-Pro.local>
2019-11-30 18:22:32 +0800

    dashboard  fix

--- src/main.js
+++ src/main.js
@@ -5,15 +5,14 @@ import router from '@/router'
 import ElementUI from 'element-ui';
 import App from './App.vue';
 import store from './store'
-
 import 'element-ui/lib/theme-chalk/index.css';
+
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
 new Vue({
   el: '#app',
   store,
-  //让vue知道我们的路由规则
-  router: router, //可以简写router
+  router, //可以简写router
   render: c => c(App),
 })
